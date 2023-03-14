dad0323c6aed7cb3916ac9e3dbf12608553b007f
jaustriattec <austriajohnrobert33@gmail.com>
2023-03-14 23:54:03 +0800

    add loan update

--- src/components/loans/addLoan.js
+++ src/components/loans/addLoan.js
@@ -1,12 +1,36 @@
+import { useState } from 'react';
 import useFetch from '../../customHooks/useFetch';
 
 const AddLoan = () => {
+    const loanData = {
+        borrowerId : "",
+        amount : "",
+        interestRate : "",
+        interestAmount : "",
+        term: "",
+        monthlyDue : "",
+        startDate : "",
+        dueDate : "",
+        balance : "",
+        payments : "",
+        status : ""
+    }
+    const [loan , setLoan] = useState(loanData)
     const {data, loading} = useFetch('/borrowers')
-    if(loading)return <h1>Loading...</h1>    
+    if(loading)return <h1>Loading...</h1>   
 
     function handleChange(e){
-
+        setLoan({...loan,[e.target.id]:e.target.value})
+       
     }
+    function computeMonthlyInterestAmount(){
+        if(loan.amount && loan.interestRate){
+            let interestAmount = parseInt(loan.amount) * (parseInt(loan.interestRate)/100)
+            setLoan({...loan,interestAmount})           
+        }
+        return loan.interestAmount
+    } 
+    console.log(loan)
 
     return ( 
         <>   
@@ -20,9 +44,11 @@ const AddLoan = () => {
                     <div className="col-3">
                     <div className="form-group">
                                 <label>Borrower</label>
-                                <select className="form-control" id="borrower">
+                                <select className="form-control" id="borrowerId" value={loan.borrowerId} onChange={handleChange}>
+                                <option value="">Select borrower...</option>
                                     {data.map(borrower => {
-                                        return <option key={borrower._id}>{borrower.name.firstName + " " + borrower.name.lastName}</option>
+                                        const fullName = borrower.name.firstName + " " + borrower.name.lastName
+                                        return <option value={borrower._id} key={borrower._id}>{fullName}</option>
                                     })
                                     }
                                 </select>
@@ -30,27 +56,27 @@ const AddLoan = () => {
                     </div>
                     <div className="col-3">
                         <div className="form-group">
-                            <label>Amount</label>
-                                <select className="form-control" id="amount">
-                                    <option value="" selected disabled hidden>Select amount...</option>
-                                    <option value="1">Php 1000</option>
-                                    <option value="2">Php 2000</option>
-                                    <option value="3">Php 3000</option>
-                                    <option value="4">Php 5000</option>
-                                    <option value="5">Php 10000</option>
+                            <label>Amount (PHP)</label>
+                                <select className="form-control" value={loan.amount} id="amount" onChange={handleChange}>
+                                    <option value="">Select amount...</option>
+                                    <option value="1000">1000</option>
+                                    <option value="2000">2000</option>
+                                    <option value="3000">3000</option>
+                                    <option value="5000">5000</option>
+                                    <option value="10000">10000</option>
                                 </select>
                         </div>
                     </div>    
                     <div className="col-3">
                         <div className="form-group">
                             <label>Term (Month)</label>
-                            <select className="form-control" id="amount">
-                                    <option value="" selected disabled hidden>Select term...</option>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>6</option>
-                                    <option>12</option>
+                            <select className="form-control" id="term" value={loan.term} onChange={handleChange}>
+                                    <option value="">Select term...</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="6">6</option>
+                                    <option value="12">12</option>
                                 </select>
                         </div>
                     </div>            
@@ -61,25 +87,25 @@ const AddLoan = () => {
                     <div className="col-3">
                         <div className="form-group">
                         <label>Monthly Interest Rate</label>
-                                <select className="form-control" id="amount">
-                                    <option value="" selected disabled hidden>Select interest...</option>
-                                    <option>0%</option>
-                                    <option>4%</option>
-                                    <option>6%</option>
-                                    <option>10%</option>
+                                <select className="form-control" id="interestRate" value={loan.interestRate} onChange={handleChange}>
+                                    <option value="">Select interest...</option>
+                                    <option value="0">0%</option>
+                                    <option value="4">4%</option>
+                                    <option value="6">6%</option>
+                                    <option value="10">10%</option>
                                 </select>
                         </div>
                     </div>
                     <div className="col-3">
                         <div className="form-group">
                         <label>Monthly Interest Amount</label>
-                        <input type="text" className="form-control" id="interestAmount" readOnly/>
+                        <input type="text" className="form-control" id="interestAmount" value={computeMonthlyInterestAmount} readOnly/>
                         </div>
                     </div>   
                     <div className="col-3">
                         <div className="form-group">
                         <label>Monthly Due</label>
-                        <input type="text" className="form-control" id="interestAmount" readOnly/>
+                        <input type="text" className="form-control" id="monthlyDue" value={loan.monthlyDue} readOnly/>
                         </div>
                     </div>                      
                 </div>
@@ -88,7 +114,7 @@ const AddLoan = () => {
                     <div className="col-3">
                         <div className="form-group">
                             <label>Start Date</label>
-                            <input type="text" class="form-control"/>                        
+                            <input type="text" className="form-control"/>                        
                         </div>
                     </div>
                     <div className="col-3">
